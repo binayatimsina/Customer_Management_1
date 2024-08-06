c4aa164d470d41e5d8950d8e24778f7e6e17ac94
Utkarsh <utkarshnsr@gmail.com>
2024-08-06 15:04:45 +0000

    changed name of class

--- src/main/java/com/example/demo/CustomerService.java
+++ src/main/java/com/example/demo/CustomerService.java
@@ -3,10 +3,10 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CUstomerService {
+public class CustomerService {
     private List<Customer> list;
 
-    public CUstomerService() {
+    public CustomerService() {
         list = new ArrayList<>();
         list.add(new Customer(1, "John Doe"));
         list.add(new Customer(2, "Jane Adams"));
