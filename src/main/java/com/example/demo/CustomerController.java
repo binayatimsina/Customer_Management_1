96e343127c49fc40e8d6b4259e453393f84846ff
Utkarsh <utkarshnsr@gmail.com>
2024-08-06 17:52:58 +0000

    add deleteCustomer method

--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,5 +41,11 @@ public class CustomerController {
         String updatedName = body.get("name");
         customerService.updateCustomer(updatedName, id);
         return getAllCustomers(); 
+    }
+
+    @DeleteMapping("/{id}")
+    public List<Customer> deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+        return getAllCustomers();
     }
 }
