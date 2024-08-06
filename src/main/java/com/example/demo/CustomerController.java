cd7729c0634348915ebfbc5c8f817b0f039eac94
binayatimsina <binayatimsina2@gmail.com>
2024-08-06 17:37:51 +0000

    Add RestController

--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,19 +18,24 @@ public class CustomerController {
     @Autowired public CustomerService customerService;
 
     @GetMapping("")
-    public String customer() {
-        return "Welcome to Customer Management API";
-    }
-
-    @GetMapping("/allCustomers") 
     public List<Customer> getAllCustomers() {
         return customerService.getAllCustomers();
     }
 
-    @PostMapping("/createCustomer")
-    public List<Customer> createCustomer(@RequestBody String name) {
+    @GetMapping("") 
+    public Customer getCustomerById(@RequestParam Long id) {
+        return customerService.getCustomerByID(id);
+    }
 
+    @PostMapping("/createCustomer")
+    public List<Customer> createCustomer(@RequestBody Map<String, String> body) {
+        String name = body.get("name");
         customerService.addCustomer(name);
         return getAllCustomers(); 
+    }
+
+    @PutMapping("")
+    public List<Customer> updateCustomerList() {
+        
     }
 }
