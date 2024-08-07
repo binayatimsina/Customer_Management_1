ca79eb9151bf009862a4a1ab974f68d644e3ac55
Utkarsh <utkarshnsr@gmail.com>
2024-08-07 20:40:48 +0000

    mock

---
+++ src/test/java/com/example/demo/CustomerServiceTest.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomerServiceTest {
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @InjectMocks
+    private CustomerService customerService;
+
+    @Test
+    public void testGetAllCustomers() {
+        
+    }
+}
